ceabe861c62bee5b59adf7afe7a24d640a61a91f
Olga Bulat <obulat@gmail.com>
2021-01-01 11:20:15 +0300

    Fix base model for post parameter

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from fastapi import FastAPI
 from annotator import Annotator
 from starlette.responses import JSONResponse
 from starlette.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 
 annotator = Annotator()
 app = FastAPI()
 origins = ["*"]
 app.add_middleware(CORSMiddleware, allow_origins=origins)
 
+class Text(BaseModel):
+    text: str
+
 @app.post('/annotate_text')
-def annotate_text(text: str):
-    return JSONResponse(annotator.annotate(text))
+def annotate_text(text: Text):
+    return JSONResponse(annotator.annotate(text.text))
 
 @app.get('/', status_code=200)
 async def healthcheck():
